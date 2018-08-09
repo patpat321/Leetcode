c492f2758bd2b4bf3b5b55f5f6dd522be23417a0
patpat321 <33772517+patpat321@users.noreply.github.com>
2018-08-09 14:23:16 +0800

    Update 18.py

--- Hash Table/18.py
+++ Hash Table/18.py
@@ -18,3 +18,26 @@ A solution set is:
 ]
 
 """
+class Solution:
+    def fourSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[List[int]]
+        """
+        sum2 = {}
+        res = []
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                if nums[i] + nums[j] not in sum2:
+                    sum2[nums[i] + nums[j]] = [[i,j]]
+                else:
+                    sum2[nums[i] + nums[j]].append([i,j])
+        for m in range(len(nums)):
+            for n in range(m+1, len(nums)):
+                if target-(nums[m] + nums[n]) in sum2:
+                    for i, j in sum2[target-(nums[m] + nums[n])]:
+                        if i!=m and i!=n and j!=m and j!=n:
+                            if sorted([nums[m], nums[n], nums[i], nums[j]]) not in res:
+                                res.append(sorted([nums[m], nums[n], nums[i], nums[j]]))
+        return res
