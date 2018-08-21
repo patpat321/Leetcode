11185d0646de7621b50f781e0166e1b8bc5a15ef
Qi Feng <33772517+patpat321@users.noreply.github.com>
2018-08-21 14:42:21 +0800

    Create 104.py

---
+++ Depth-first Search/104.py
@@ -0,0 +1,46 @@
+"""
+Given a binary tree, find its maximum depth.
+
+The maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+
+Note: A leaf is a node with no children.
+
+Example:
+
+Given binary tree [3,9,20,null,null,15,7],
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+return its depth = 3.
+"""
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def maxDepth(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        if not root:
+            return 0
+        stack = [(root, 1)]
+        depth = 1
+        while stack:
+            node, level = stack.pop()
+            if node.left:
+                stack.append((node.left, level+1))
+            if node.right:
+                stack.append((node.right, level+1))
+            depth = max(depth, level)
+        return depth
+"""
+Traverse through the tree by DFS.
+"""
