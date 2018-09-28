662fd5ac43a3551862e9339d3f24fcf0bd2469c8
Qi Feng <33772517+patpat321@users.noreply.github.com>
2018-09-28 19:01:22 -0400

    Create 300.py

---
+++ Dynamic Programming/300.py
@@ -0,0 +1,31 @@
+"""
+Given an unsorted array of integers, find the length of longest increasing subsequence.
+
+Example:
+
+Input: [10,9,2,5,3,7,101,18]
+Output: 4 
+Explanation: The longest increasing subsequence is [2,3,7,101], therefore the length is 4. 
+Note:
+
+There may be more than one LIS combination, it is only necessary for you to return the length.
+Your algorithm should run in O(n2) complexity.
+Follow up: Could you improve it to O(n log n) time complexity?
+"""
+class Solution:
+    def lengthOfLIS(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if not nums:
+            return 0
+        leng = [1]*len(nums)
+        for i in range(len(nums)):
+            for j in range(i, -1, -1):
+                if nums[i] > nums[j]:
+                    leng[i] = max(leng[j]+1, leng[i])
+        return max(leng)
+"""
+DP to record the longest path so far for each node.
+"""
