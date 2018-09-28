c76adab8e17a1b8f8a78ce37c54cfd0d153ae942
Qi Feng <33772517+patpat321@users.noreply.github.com>
2018-09-27 23:45:07 -0400

    Create 258.py

---
+++ Math/258.py
@@ -0,0 +1,25 @@
+"""
+Given a non-negative integer num, repeatedly add all its digits until the result has only one digit.
+
+Example:
+
+Input: 38
+Output: 2 
+Explanation: The process is like: 3 + 8 = 11, 1 + 1 = 2. 
+             Since 2 has only one digit, return it.
+Follow up:
+Could you do it without any loop/recursion in O(1) runtime?
+"""
+class Solution:
+    def addDigits(self, num):
+        """
+        :type num: int
+        :rtype: int
+        """
+        m = str(num)
+        while len(m) != 1:
+            res = 0
+            for i in range(len(m)):
+                res += int(m[i])
+            m = str(res)
+        return int(m)
